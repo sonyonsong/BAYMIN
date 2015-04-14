466159905325d7e5d3b9083e0886aa9d96f2d22f
Elisha Lee <esl2131@columbia.edu>
2015-04-14 16:55:23 -0400

    finished up waving and bumping. tested and works. need to integrate with Elishas python script

--- challenge_problem_1/src/move_arm_random/src/move_arm_random.py
+++ challenge_problem_1/src/move_arm_random/src/move_arm_random.py
@@ -16,10 +16,28 @@ if __name__ == '__main__':
     robot = moveit_commander.RobotCommander()
     scene = moveit_commander.PlanningSceneInterface()
     right_arm_group = moveit_commander.MoveGroupCommander("right_arm")
+    left_arm_group = moveit_commander.MoveGroupCommander("left_arm")
 
     rospy.sleep(3)
 
-    print "========= Generating Pose 1 ========="
+    print "========= Tuck Left Arm =========="
+
+    pose = geometry_msgs.msg.Pose()
+    pose.orientation.x = 0.478
+    pose.orientation.y = -0.491
+    pose.orientation.z = 0.547
+    pose.orientation.w = -0.47
+    pose.position.x = 0.118
+    pose.position.y = -0.113
+    pose.position.z = 0.384
+    left_arm_group.set_pose_target(pose)
+    left_arm_group.go()
+
+    ## CHECK FOR VOICE COMMAND HERE
+
+    ## OPTION 1: GREETING
+
+    print "========= Moving to Pose 1 ========="
 
     target_pose = geometry_msgs.msg.Pose()
     target_pose.orientation.x = 0
@@ -30,47 +48,50 @@ if __name__ == '__main__':
     target_pose.position.y = -0.238729
     target_pose.position.z = 1.20937
     right_arm_group.set_pose_target(target_pose)
-
-    print "========= Moving to Pose 1 ========="
-
     right_arm_group.go()
 
+    print "========== Waving... ========="
+
+    target_pose.position.y += 0.1
+    right_arm_group.set_pose_target(target_pose)
+    right_arm_group.go()
+
+    for i in range(2):
+
+        target_pose.position.y -= 0.2
+        right_arm_group.set_pose_target(target_pose)
+        right_arm_group.go()
+
+        target_pose.position.y += 0.2
+        right_arm_group.set_pose_target(target_pose)
+        right_arm_group.go()
+
+    target_pose.position.y -= 0.1
+    right_arm_group.set_pose_target(target_pose)
+    right_arm_group.go()
+    
+    # OPTION 2: FIST BUMP
+
+    print "========= Moving to Pose 2 =========="
+    
     target_pose.orientation.x = 0
     target_pose.orientation.y = -0.629019
     target_pose.orientation.z = 0
-    target_pose.orientation.w = 0.77739
-    target_pose.position.x = 0.823748
+    target_pose.orientation.w = 0.97739
+    target_pose.position.x = 0.423748
     target_pose.position.y = -0.238729
     target_pose.position.z = 1.20937
     right_arm_group.set_pose_target(target_pose)
-
-    print "========= Generating Cartesian Path =========="
-
-    waypoints = []
-    
-    # start with current pose
-    waypoints.append(right_arm_group.get_current_pose().pose)
-    print waypoints[0]
-    
-    # first move: orient gripper and move forward (+x)
-    wpose = geometry_msgs.msg.Pose()
-    wpose.orientation.x = waypoints[0].orientation.x
-    wpose.orientation.y = waypoints[0].orientation.y
-    wpose.orientation.z = waypoints[0].orientation.z
-    wpose.orientation.w = waypoints[0].orientation.w
-    wpose.position.x = waypoints[0].position.x
-    wpose.position.y = waypoints[0].position.y + 0.5
-    wpose.position.z = waypoints[0].position.z
-
-    waypoints.append(copy.deepcopy(wpose))
-
-    # compute cartesian path
-
-    (plan2, fraction) = right_arm_group.compute_cartesian_path(waypoints, 0.01, 0.0)
-    
-    rospy.sleep(3)
-    print "========= Moving along Cartesian Path =========="
     right_arm_group.go()
-    rospy.sleep(3)
 
-    IPython.embed()
+    print "========== Bumping... ========="
+
+    target_pose.position.x += 0.1
+    right_arm_group.set_pose_target(target_pose)
+    right_arm_group.go()
+
+    target_pose.position.x -= 0.1
+    right_arm_group.set_pose_target(target_pose)
+    right_arm_group.go()
+
+    #IPython.embed()
